05820c174d2c08f4888b4957edaafc774e6c8269
kandys24 <ccumbica24@gmail.com>
2025-03-31 21:02:35 +0100

    add placeholder Novo nome de categoria

--- src/components/Admin/MenuEditor.jsx
+++ src/components/Admin/MenuEditor.jsx
@@ -103,7 +103,7 @@ const MenuEditor = () => {
           <input
             type="text"
             className="flex-1 px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-amber-500"
-            placeholder="New category name"
+            placeholder="Novo nome de categoria"
             value={newCategory}
             onChange={(e) => setNewCategory(e.target.value)}
           />
